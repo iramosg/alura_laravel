d3429311072a8e3f7c5cc4dd775b91d54cd603b3
Igor Ramos <igor.goncalves@sunsetcom.com.br>
2017-10-02 10:18:29 -0300

    Ajustes no CRUD

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -2,21 +2,12 @@
 
 use Illuminate\Support\Facades\DB;
 
-class ProdutoController extends Controller{
+class ProdutoController extends Controller {
 
 	public function lista(){
 
-		$html = '<h1>Listagem de produtos com Laravel </h1>';
+		$produtos = DB::select('select * from produtos');
 
-		$html .= '<ul>';
-
-		$produto = DB::select('select * from produtos');
-
-		foreach ($produto as $p) {
-			$html .= '<li> Nome: '. $p->nome .' Descrição: '. $p->descricao .'</li>';
-		}
-		$html .= '</ul>';
-
-		return $html;
+		return view('listagem')->withProdutos($produtos);
 	}
 }
